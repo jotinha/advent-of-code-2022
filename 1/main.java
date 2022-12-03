fa637b8d0661d52220b6bf4ef1c198a294f3dd31
jotinha <dev@jgsousa.com>
2022-12-03 23:05:22 +0000

    1: part 2

--- 1/main.java
+++ 1/main.java
@@ -1,27 +1,31 @@
 import java.io.*;
 import java.lang.Math;
+import java.util.Arrays;
 
 class Puzzle1 {
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		FileReader fr = new FileReader("input");
-		BufferedReader br = new BufferedReader(fr);
-		String line;
-		
-		int calories = 0;
-		int maxCalories = 0;
+        FileReader fr = new FileReader("input");
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        
+        int calories = 0;
+        int maxCalories = 0;
+        int[] topCalories = new int[3];
 
-		while ((line = br.readLine()) != null) {
+        while ((line = br.readLine()) != null) {
 
-			if (line.trim().isEmpty()) {
-				calories = 0;
-			} else {
-				calories += Integer.parseInt(line.trim());				
-                maxCalories = Math.max(calories, maxCalories);
-			}
-			// debug
-			System.out.printf("%s %d %d\n", line, calories, maxCalories);
-		}
-		System.out.printf("%d\n", maxCalories);
-	}
-}	
+            if (line.trim().isEmpty()) {
+                calories = 0;
+            } else {
+                calories += Integer.parseInt(line.trim());                
+                if (calories > topCalories[0]) {
+                    topCalories[0] = calories;
+                    Arrays.sort(topCalories);
+                }
+            }
+            System.out.printf("%s %d %s\n", line, calories, Arrays.toString(topCalories));
+        }
+        System.out.printf("%d\n", Arrays.stream(topCalories).sum());
+    }
+}    
