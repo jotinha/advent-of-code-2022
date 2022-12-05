e0be0a84d4a22d2db92f59bde4807e8a4a9345b4
jotinha <dev@jgsousa.com>
2022-12-05 00:18:28 +0000

    4: refactor

--- 4/main.sql
+++ 4/main.sql
@@ -8,25 +8,21 @@ from data
 ),
 
 data3 as (
-select 
-    cast(substr(a,1,a_mid-1) as int) as a_start, 
-    cast(substr(a,a_mid+1) as int)   as a_end,
-    cast(substr(b,1,b_mid-1) as int) as b_start, 
-    cast(substr(b,b_mid+1) as int)   as b_end
+select  
+    cast(substr(a,1,a_mid-1) as int) as a1,
+    cast(substr(a,a_mid+1) as int)   as a2,
+    cast(substr(b,1,b_mid-1) as int) as b1, 
+    cast(substr(b,b_mid+1) as int)   as b2
 from data2
-)
+),
 
-select 
-    sum(case 
-        when (a_start <= b_start and a_end >= b_end) then 1
-        when (b_start <= a_start and b_end >= a_end) then 1
-        else 0
-    end) as total_a,
-    
-    sum(case
-        when (a_end < b_start) then 0
-        when (b_end < a_start) then 0
-        else 1
-    end) as total_b
+stats as (
+select count(1) as total,
+       sum((a2 < b1) | (a1 > b2)) as no_overlaps,
+       sum((a1 >= b1) & (a2 <= b2)) as a_in_b,
+       sum((b1 >= a1) & (b2 <= a2)) as b_in_a,
+       sum((a1==b1) & (a2==b2)) as equal
+from data3)
 
-from data3
+select a_in_b + b_in_a - equal, total - no_overlaps 
+from stats
