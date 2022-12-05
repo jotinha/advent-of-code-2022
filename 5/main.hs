64486eea5e18ad0d6ddcff69a47d0723d7c7879d
jotinha <dev@jgsousa.com>
2022-12-05 18:37:19 +0000

    5: cleanup and simplify

--- 5/main.hs
+++ 5/main.hs
@@ -7,72 +7,67 @@ type Instruction = (Int, Int, Int)
 type Stack = [Char]
 data Version = A | B deriving (Eq)
 
-maybeAt :: Int -> [a] -> Maybe a
-maybeAt _ [] = Nothing
-maybeAt i (x:xs) | i > 0 = maybeAt (i-1) xs
-                 | i == 0 = Just x
-                 | i < 0 = Nothing
+parseInt :: String -> Maybe Int
+parseInt s = readMaybe s
 
+parseAllNumbers :: String -> [Int]
+parseAllNumbers line = catMaybes $ map parseInt $ words line 
 
-parseInt :: String -> Int
-parseInt s = fromMaybe 0 (readMaybe s :: Maybe Int)
-
-getAllNumbers :: String -> [Int]
-getAllNumbers line = filter (>0) $ map parseInt $ words line 
-
-parseInstruction :: String -> Instruction 
-parseInstruction = toTuple . getAllNumbers
-    where toTuple (x:y:z:rest) = (x,y,z)
-
-isin xs x = any (== x) xs
-iswhitespace = isin " \n\t\r"
+iswhitespace c = elem c " \n\t\r"
 trim = dropWhileEnd iswhitespace . dropWhile iswhitespace
 
-header text = takeWhile (not.null.trim) text 
-body text = dropWhile null $ dropWhile (not.null) $ map trim text
+parseHeader = takeWhile (not.null.trim)
+parseBody = dropWhile (null.trim) . dropWhile (not.null.trim)
 
 getColumn :: Int -> [[a]] -> [a]
-getColumn i xs = catMaybes $ map (maybeAt i) xs
+getColumn 0 = map head 
+getColumn i = getColumn (i-1) . map tail
 
-getStack :: Int -> [String] -> Stack
-getStack i lines = trim $ getColumn ((i-1)*4+1) lines
+parseInstruction :: String -> Instruction 
+parseInstruction = toTuple . parseAllNumbers
+    where toTuple (x:y:z:rest) = (x,y,z)
+
+parseInstructions :: [String] -> [Instruction]
+parseInstructions = map parseInstruction
+
+parseStack :: Int -> [String] -> Stack
+parseStack i lines = trim $ getColumn ((i-1)*4+1) lines
 
 parseStacks :: [String] -> [Stack]
-parseStacks ls = map (\i -> getStack i rows) [1..maxIdx] 
+parseStacks ls = [parseStack i rows | i <- [1..maxIdx]]
     where 
         rows = init ls
-        maxIdx = last $ getAllNumbers $ last ls 
-        
-parseInputData :: String -> ([Instruction],[Stack])
-parseInputData contents = (map parseInstruction (body ls), parseStacks (header ls))
-    where ls = lines contents
+        maxIdx = last $ parseAllNumbers $ last ls 
 
-addToStack :: Stack -> Stack -> Stack
-addToStack what dest = dest ++ what
-
-execute :: Instruction -> [Stack] -> Version -> [Stack]
-execute (n, from, to) stacks version = [newStackAt i | i <- [1..(length stacks)]]
+execute :: Version -> Instruction -> [Stack] -> [Stack]
+execute version (n, from, to) stacks = map transform [1..(length stacks)]
     where 
         toMove = take n $ oldStackAt from
         toMove' | version == A = reverse toMove
                 | version == B = toMove
         oldStackAt i = stacks !! (i-1)
-        newStackAt i | i == from = drop n (oldStackAt i) 
-                     | i == to = toMove' ++ (oldStackAt i)
-                     | otherwise = oldStackAt i
+        transform i | i == from = drop n (oldStackAt i)
+                    | i == to   = toMove' ++ (oldStackAt i)
+                    | otherwise = oldStackAt i
     
-executeMany :: [Instruction] -> [Stack] -> Version -> [Stack]
-executeMany insts stacks version = foldl (\acc inst -> execute inst acc version) stacks insts
+executeMany :: Version -> [Instruction] -> [Stack] ->  [Stack]
+executeMany version insts stacks = foldl executor stacks insts
+    where executor = flip (execute version)
 
 topOfStacks :: [Stack] -> [Char]
-topOfStacks = map head
+topOfStacks = getColumn 0 
 
-resultsPartA contents = topOfStacks $ executeMany instructions stacks A
-    where (instructions, stacks) = parseInputData contents
-resultsPartB contents = topOfStacks $ executeMany instructions stacks B
-    where (instructions, stacks) = parseInputData contents
+compute :: Version -> String -> [Char] 
+compute version contents = topOfStacks $ executeMany version instructions stacks
+    where 
+        ls = lines contents
+        instructions = parseInstructions $ parseBody ls
+        stacks = parseStacks $ parseHeader ls
 
 main = do
     contents <- readFile "input"
-    putStrLn $ (resultsPartA contents) ++ "," ++ (resultsPartB contents)
+    putStr $ compute A contents
+    putStr ","
+    putStr $ compute B contents
+    putStr "\n"
     
