2ff0097f652ce2e135646a07d1a52f8253069c81
jotinha <dev@jgsousa.com>
2022-12-10 23:21:19 +0000

    7: better implementation

--- 7/main.py
+++ 7/main.py
@@ -1,43 +1,35 @@
-from dataclasses import dataclass
+from collections import defaultdict
 
-@dataclass
-class Dir:
-    name: str
-    parent: "Dir" = None
-    children: ["Dir"] = None 
-    own_size: int = 0
+def parent(wd): 
+    return wd[:-1]
+def child(wd, name): 
+    return (*wd, name)
+def parents(d): 
+    while d := parent(d): 
+        yield d
 
-    def add_child(self, c: "Dir"):
-        self.children.append(c)
-
-    @property
-    def total_size(self):
-        return self.own_size + sum(c.total_size for c in self.children) 
-
-    def walk(self):
-        for c in self.children:
-            yield c
-            yield from c.walk()
-
-root = cwd = Dir("root", None, [])
+sizes = defaultdict(lambda : 0)   
+cwd = ()
 
 for line in open("input"): 
     # print(line, cwd)
     match line.split():
-        case ["$", "cd", ".."]:
-            cwd = cwd.parent
+        case ["$", "cd", ".."]: 
+            cwd = cwd[:-1]
+            #cwd = cwd.parent
         case ["$", "cd", name]:
-            cwd_ = Dir(name, cwd, [])
-            cwd.add_child(cwd_)
-            cwd = cwd_
+            cwd = (*cwd, name)
         case ["$", "ls"]:
             pass
         case ["dir", _]:
             pass
         case [size, _]:
-            cwd.own_size += int(size)
+            size = int(size)
+            sizes[cwd] += size # update own size
+            for d in parents(cwd): # update parents as well
+                sizes[d] += size
 
-ans1 = sum(filter(lambda s: s < 100_000, map(lambda d: d.total_size, root.walk())))
+ans1 = sum(s for s in sizes.values() if s < 100_000)
 ans2 = "TODO"
 
 print(f"{ans1},{ans2}")
