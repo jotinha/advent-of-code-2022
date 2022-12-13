c08da99ac925049e8603985fbf2505f07650dbbb
jotinha <dev@jgsousa.com>
2022-12-13 15:51:29 +0000

    9: move head

--- 9/main.rb
+++ 9/main.rb
@@ -1,7 +1,32 @@
 Cmd = Struct.new(:dir, :steps)
+Pos = Struct.new(:x, :y)
 
 cmds = File.readlines("test").map { |line|
     x  = line.strip.split
     Cmd.new(x[0].to_sym, x[1].to_i)
 }
-puts cmds
+
+head = tail = Pos.new(0,0)
+
+def move!(cmd, pos)
+    while cmd.steps > 0 do
+        case cmd.dir
+            when :R
+                pos.x += 1
+            when :L
+                pos.x -= 1
+            when :U
+                pos.y += 1
+            when :D
+                pos.y -= 1
+        end
+        cmd.steps-=1
+    end
+end
+
+cmds.each do |cmd| 
+    puts cmd
+    move!(cmd, head)
+    puts head
+end
+
