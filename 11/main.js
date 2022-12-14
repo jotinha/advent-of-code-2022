96c075f3eae753e5d0c130b5bfc08298643f2a92
jotinha <dev@jgsousa.com>
2022-12-14 17:11:23 +0000

    11: compute instructions

--- 11/main.js
+++ 11/main.js
@@ -11,7 +11,7 @@ function makeMonkey(m, i) {
 
     return {
         idx: idx,
-        starting: m[2].split(',').map(x => parseInt(x)),
+        items: m[2].split(',').map(x => parseInt(x)),
         operation: m[3],
         by: parseInt(m[4]) || 'old',
         divisible: parseInt(m[5]),
@@ -19,7 +19,40 @@ function makeMonkey(m, i) {
         falseTo: parseInt(m[7])
     }    
 }
+function updateWorry(old, operation, by) {
+    by = by == 'old' ? old : by
+    switch (operation) {
+        case '*':
+            return old * by;
+        case '+':
+            return old + by;
+        case '-':
+            return old - by;
+        case '/':
+            return old / by;
+        default:
+            throw(`Invalid operation: ${operation}`)
+    }
+}
+
+function round(monkeys) {
+    monkeys.forEach(m => {
+        m.items.forEach(worry => {
+            newWorry = updateWorry(worry, m.operation, m.by);
+            newWorry2 = Math.floor(newWorry/3);
+            sendTo = newWorry2 % m.divisible == 0 ? m.trueTo : m.falseTo
+            console.log(`${worry}->${newWorry}->${newWorry2} to monkey ${sendTo}`);
+            monkeys[sendTo].items.push(newWorry2)
+        })
+        m.items = [] // monkey sent everything
+    })
+    
+}
+
+function printItems(monkeys) {
+    monkeys.forEach((m,i) => console.log(`${i}: ${m.items}`))
+}
 
 let monkeys = [...data.matchAll(pattern)].map(makeMonkey)
-
-console.log(monkeys)
+round(monkeys);
+printItems(monkeys)
