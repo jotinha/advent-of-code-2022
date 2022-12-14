f2bf40fb454b880a3c94edd020a16b050ac3d7cc
jotinha <dev@jgsousa.com>
2022-12-14 14:47:43 +0000

    10: refactor to do each individual step

--- 10/main.pl
+++ 10/main.pl
@@ -4,35 +4,25 @@ use warnings;
 open my $in,'<','input' or die "Could not open file";
 
 my $cycle = 1;
-my $signal = 1;
+my $x = 1;
 my $strength = 0;
-my $jump = 0;
-my $delta = 0;
+
+sub step {
+    if (($cycle + 20) % 40 == 0) {
+       $strength += $cycle*$x 
+    }
+    $cycle += 1;
+}
 
 while (<$in>) {
-    
     if (/noop/) {
-        $jump = 1;
-        $delta = 0;
+        step;
     } elsif (/addx (-?\d+)/) {
-        $jump = 2;
-        $delta = int($1);
+        step;
+        step;        
+        $x += int($1);
     } else {
         die "invalid"
     }
- 
-    my $rest = ($cycle + 20 + 1) % 40;
-    if ($rest == 1) { # 20, 40, 80...
-        $strength += $cycle*$signal;
-        #print "[$strength] ";
-    } elsif ($rest == 0 && $jump>1) { # 19, 39, 79...
-        $strength += ($cycle+1)*$signal;
-        #print "[$strength] ";
-    }
-
-    #print "$cycle: ($signal) $_";
-
-    $cycle += $jump;
-    $signal += $delta;
 }
-print $strength
+print "$strength\n"
