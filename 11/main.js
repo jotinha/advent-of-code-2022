a77c474275fbfe36fdf5bd21d3564fb14c6f0cbb
jotinha <dev@jgsousa.com>
2022-12-14 23:28:20 +0000

    11: attempted approach where we keep all rests (works in test, fails in input)

--- 11/main.js
+++ 11/main.js
@@ -11,39 +11,46 @@ function makeMonkey(m, i) {
 
     return {
         idx: idx,
-        items: m[2].split(',').map(x => parseInt(x)),
+        items: m[2].split(',').map(x => {return {val: parseInt(x)}} ),
         operation: m[3],
         by: parseInt(m[4]) || 'old',
         divisible: parseInt(m[5]),
         trueTo: parseInt(m[6]),
         falseTo: parseInt(m[7]),
-        inspected: 0
+        inspected: 0,
     }    
 }
 function updateWorry(old, operation, by) {
-    by = by == 'old' ? old : by
     switch (operation) {
         case '*':
             return old * by;
         case '+':
             return old + by;
-        case '-':
-            return old - by;
-        case '/':
-            return old / by;
         default:
             throw(`Invalid operation: ${operation}`)
     }
 }
+function computeRests(x, maxm) {
+    return [...Array(maxm+1).keys()].map(m => x % m)
+}
 
-function round(monkeys) {
+function isDivisible(item, d) {
+    return item.rests[d] == 0;
+}
+
+function round(monkeys, divideBy3) {
     monkeys.forEach(m => {
-        m.items.forEach(worry => {
-            newWorry = updateWorry(worry, m.operation, m.by);
-            newWorry2 = Math.floor(newWorry/3);
-            sendTo = newWorry2 % m.divisible == 0 ? m.trueTo : m.falseTo
-            //console.log(`${worry}->${newWorry}->${newWorry2} to monkey ${sendTo}`);
-            monkeys[sendTo].items.push(newWorry2)
+        m.items.forEach(item => {
+            item.val = updateWorry(item.val, m.operation, m.by == 'old' ? item.val : m.by);
+            if (divideBy3)
+                item.val = Math.floor(item.val/3);
+            
+            // to avoid issues with large numbers we can simply keep all possible rest values
+            item.rests = item.rests.map((r,d) => 
+                updateWorry(r % d, m.operation, (m.by == 'old' ? r : m.by) % d) % d)
+            
+            let sendTo = isDivisible(item, m.divisible) ? m.trueTo : m.falseTo
+            monkeys[sendTo].items.push(item)
         })
         m.inspected += m.items.length;
         m.items = [] // monkey sent everything
@@ -52,16 +59,18 @@ function round(monkeys) {
 }
 
 function printItems(monkeys) {
-    monkeys.forEach((m,i) => console.log(`${i}: ${m.items} (inspected ${m.inspected} total)`))
+    monkeys.forEach((m,i) => console.log(`${i}: ${m.items.map(x=>x.val)} ${m.items.map(x => x.rests[m.divisible])} (inspected ${m.inspected} total)`))
 }
 
 let monkeys = [...data.matchAll(pattern)].map(makeMonkey)
+let max_d = monkeys.map(m => m.divisible).sort().slice(-1)[0];
+monkeys.forEach(m => m.items.forEach(item => {item.rests = computeRests(item.val, max_d)}))
 
-for (let i=20; i--;) round(monkeys);
+for (let i=10000; i--;) round(monkeys, false);
 printItems(monkeys)
 
-let [i1,i2] = monkeys.map(m => m.inspected).sort((a,b) => b-a); // sort desc
-
-let ans1 = i1*i2
+let totals = monkeys.map(m => m.inspected).sort((a,b) => b-a); // sort desc
+console.log(totals)
+let ans1 = totals[0]*totals[1] 
 let ans2 = "TODO"
 console.log(`${ans1},${ans2}`);
