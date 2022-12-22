e1db7ddb75d57c7f199c823ea585cbe1779fb8ae
jotinha <dev@jgsousa.com>
2022-12-22 22:50:05 +0000

    13: haskell ref implementation using custom data type

--- 13/main.hs
+++ 13/main.hs
@@ -1,16 +1,30 @@
 import Data.Maybe
 import Text.Read
 import Text.Printf
+import Data.List
+
+data Packet = Num Int | List [Packet] deriving (Show, Eq)
 
 parseInt :: String -> Maybe Int
 parseInt s = readMaybe s
 justParseInt = fromJust.parseInt
 
-pairs :: [String] -> [(String,String)]
-pairs (left:right:_:rest) = (left,right):(pairs rest)
-pairs (left:right:[]) = [(left,right)]
+enumerate = zip [0..]
+
+parsetext :: String -> [Packet]
+parsetext contents = map (parse.snd) $ filter noteverythird $ (enumerate.lines) contents 
+    where noteverythird (i,_) = i `mod` 3 /= 2
+
+pairs :: [Packet] -> [(Packet,Packet)]
+pairs (left:right:rest) = (left,right):(pairs rest)
+pairs [] = []
 
 inner = init.tail
+
+parse :: String -> Packet
+parse "" = List []
+parse s@('[':_) = List [parse x | x <- (elems.inner) s] 
+parse s = Num $ justParseInt s
 
 findclosing s = take i s
     where
@@ -33,49 +47,44 @@ elems' acc s = elems' (x:acc) $ drop (length x+1) s
     where x = nextelem s
 elems s = reverse $ elems' [] s
 
-islist = (== '[') . head 
+tolist :: Packet -> Packet
+tolist (Num x) = List [Num x]
+tolist l = l
 
-cmps :: String -> String -> Int
-cmps left right | islist left && islist right = cmpl (l left) (l right)
-                | islist left && not (islist right) = cmpl (l left) [right]
-                | not (islist left) && islist right = cmpl [left] (l right)
-                | otherwise = cmpi (justParseInt left) (justParseInt right) 
-   where l = elems.inner 
-cmpi :: Int -> Int -> Int
-cmpi left right | left < right = -1 | left > right = 1 | otherwise = 0
+cmp' :: Packet -> Packet -> Int
+cmp' (Num l) (Num r) | l < r = -1 | l > r = 1 | otherwise = 0
+cmp' (List ls) (List rs) = case (ls,rs) of 
+                    ([],[]) -> 0
+                    ([],_) -> -1 
+                    (_,[]) -> 1
+                    (l:ls, r:rs) -> if c /= 0 then c else cmp' (List ls) (List rs)
+                        where c = cmp' l r
+cmp' l r = cmp' (tolist l) (tolist r) 
 
-cmpl [] (y:ys) = -1
-cmpl (x:xs) [] = 1
-cmpl [] [] = 0
-cmpl (x:xs) (y:ys)  | c == 0 = cmpl xs ys
-                    | otherwise = c
-    where c = cmps x y
+solve1 :: [Packet] -> Int
+solve1 packets = sum [i+1 | (i,(a,b)) <- (enumerate (pairs packets)), correctorder a b]
 
-solve1 pairs = sum [i | (i,r) <- (zip [1..] res), r == -1]
-    where res = map (\(a, b) -> cmps a b ) pairs 
+correctorder p1 p2 = cmp' p1 p2 == -1
+incorrectorder p1 p2 = cmp' p1 p2 == 1
 
-correctorder p1 p2 = cmps p1 p2 == -1
-incorrectorder p1 p2 = cmps p1 p2 == 1
-
-sortpackets :: [String] -> [String]
+sortpackets :: [Packet] -> [Packet]
 sortpackets (p:ps) = lt++[p]++gt
     where lt = sortpackets $ filter (incorrectorder p) ps
           gt = sortpackets $ filter (correctorder p) ps
 sortpackets [] = []
 
-flatten :: [(String,String)] -> [String]
-flatten ((a,b):xs) = a:b:(flatten xs)
-flatten [] = []
+dividers = map parse ["[[2]]","[[6]]"] 
+isdivider p = p `elem` dividers
 
-solve2 :: [(String,String)] -> Int 
-solve2 pairs = i1*i2
+solve2 :: [Packet] -> Int 
+solve2 packets = (i1+1)*(i2+1)
     where
-        dividers = ["[[2]]","[[6]]"] 
-        packets = sortpackets $ (flatten pairs) ++ dividers 
-        ((i1,_):(i2,_):_) = filter (\(i, s) -> s `elem` dividers) $ zip [1..] packets 
+        packets' = sortpackets $ packets ++ dividers
+        ((i1,_):(i2,_):_) = filter (isdivider.snd) $ enumerate packets' 
 
 main = do
     contents <- readFile "input"
-    let ans1 = solve1 $ pairs $ lines contents 
-    let ans2 = solve2 $ pairs $ lines contents
+    let packets = parsetext contents
+    let ans1 = solve1 packets 
+    let ans2 = solve2 packets 
     putStrLn $ (show ans1)++","++(show ans2) 
