7459f757ca3ee05280f37f28a72417f35b47eee9
jotinha <dev@jgsousa.com>
2022-12-25 21:17:32 +0000

    14: build rock

--- 14/main.f95
+++ 14/main.f95
@@ -1,6 +1,6 @@
 program hello
     implicit none
-    integer :: world(520,520) 
+    integer :: world(520,10) 
     call buildworld(world)
 
 contains
@@ -10,15 +10,23 @@ contains
         character (len=128) :: line
         integer :: io
         integer :: points(256)
-        integer :: n
-
+        integer :: n,i 
+        
+        world = 0
         open(1, file='test', status='old', action='read')
         do
             read(1,"(A70)",iostat=io) line
             if (io/= 0) exit
             n = parse(line, points) 
-            print *, n, points(:n*2)
+            do i=1,n+1,2
+                call fillline(world,points(i:i+1),points(i+2:i+3))
+                !print *,points(i),points(i+2),points(i+1),points(i+3)
+                 
+            end do
+            !print *,"next"
+            !print *, n, points(:n*2)
         end do
+        call draw(transpose(world))
         close(1)
     end subroutine 
 
@@ -39,5 +47,48 @@ contains
     
     end function
 
+    subroutine fillline(world,p1,p2)
+        implicit none
+        integer, dimension(2), intent(in) :: p1,p2
+        integer, dimension(:,:), intent(inout) :: world
+        integer :: i1,i2,j1,j2 
+        i1 = min(p1(1),p2(1))
+        i2 = max(p1(1),p2(1))
+        j1 = min(p1(2),p2(2))
+        j2 = max(p1(2),p2(2))
+        world(i1:i2,j1:j2) = 1
 
+    end subroutine
+
+    subroutine draw(world)
+        implicit none
+        integer, dimension(:,:), intent(in) :: world
+        integer :: i,j
+
+        open(2, file="world", action="write")
+        do i = 1,size(world,1)
+            do j=1,size(world,2)
+                write(2,fmt='(1a)', advance="no") pict(world(i,j)) 
+            end do
+            write (2,*) ""
+        end do
+        close(2)
+    end subroutine
+
+    character function pict(pixel)
+        implicit none
+        integer, intent(in) :: pixel        
+
+        select case (pixel)
+            case (0) 
+                pict = '.'
+            case (1) 
+                pict = '#'
+            case (2) 
+                pict = '@'
+            case default
+                pict = '?'
+        end select 
+    end function pict
+    
 end program
