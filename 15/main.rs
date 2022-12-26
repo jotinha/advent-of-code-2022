6bd015b44c7ae947c1a1e6113b5da13767299350
jotinha <dev@jgsousa.com>
2022-12-26 12:57:20 +0000

    15: solution for part 1

--- 15/main.rs
+++ 15/main.rs
@@ -1,9 +1,9 @@
 use std::fs;
 
 struct Point {x:i32,y:i32}
-struct Pair {sensor: Point, beacon: Point}
+struct Sensor {pos: Point, beacon: Point, coverage: i32 } 
 
-fn parse(line: &str) -> Pair {
+fn parse(line: &str) -> Sensor {
 
     let ns : Vec<i32> = line
         .split(['=',',',':'])
@@ -11,11 +11,12 @@ fn parse(line: &str) -> Pair {
         .filter(|x| x.is_ok())
         .map(|x| x.unwrap()).
         collect();
-    Pair {
-        sensor: Point {x:ns[0], y:ns[1]},
-        beacon: Point {x:ns[2], y:ns[3]}
-    }
+    let s = Point {x: ns[0], y: ns[1] };
+    let b = Point {x: ns[2], y: ns[3] };
+    let d = dist(&s,&b);
+    Sensor {pos: s, beacon: b, coverage: d } 
 }
+
 
 fn splitlines(content: &str) -> Vec<&str> {
     content 
@@ -26,9 +27,35 @@ fn splitlines(content: &str) -> Vec<&str> {
 
 }
 
-fn main() {
-    let content = fs::read_to_string("test").unwrap();
-    let lines = splitlines(&content);
-    let pairs:Vec<_> = lines.into_iter().map(parse).collect();
+fn dist(a: &Point, b: &Point) -> i32 { (a.x-b.x).abs() + (a.y-b.y).abs() }
 
+fn iscoveredby(p: &Point, s: &Sensor) -> bool { 
+    dist(p,&s.pos) <= s.coverage
+}
+fn iscoveredbyany(p: &Point, sensors: &Vec<Sensor>) -> bool {
+    sensors.iter().any(|s| iscoveredby(p,s))    
+}
+fn beaconat(p: &Point, sensors: &Vec<Sensor>) -> bool {
+    sensors.iter().any(|s| s.beacon.x == p.x && s.beacon.y == p.y)
+}
+
+fn getxrange(y: i32, sensors: &Vec<Sensor>) -> Vec<Point> { 
+    let xmin = sensors.iter().map(|s| s.pos.x - s.coverage).min().unwrap();
+    let xmax = sensors.iter().map(|s| s.pos.x + s.coverage).max().unwrap();
+    (xmin-1..xmax+1).map(|x| Point {x:x,y:y}).collect()
+
+}
+
+fn main() {
+    let content = fs::read_to_string("input").unwrap();
+    let lines = splitlines(&content);
+    let sensors:Vec<Sensor> = lines.into_iter().map(parse).collect();
+    
+    let y = 2000000;
+    let ans1 = getxrange(y,&sensors)
+        .iter()
+        .filter(|p| iscoveredbyany(p,&sensors))
+        .filter(|p| !beaconat(p,&sensors))
+        .count();
+    println!("{},{}",ans1,"TODO");
 }
