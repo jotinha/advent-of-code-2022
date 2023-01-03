13e7845b952bb468b2c2d5b42ced0fbf85d9f216
jotinha <dev@jgsousa.com>
2023-01-03 00:25:21 +0000

    16: parse input

--- 16/main.ts
+++ 16/main.ts
@@ -1,1 +1,18 @@
-console.log(`Hello world`);
+import { readFileSync } from 'fs';
+
+interface Valve {
+    name: string;
+    flow: number;
+    to: number[];
+}
+
+let parseLine = function(line: string): Valve {
+    const re = /Valve ([A-Z][A-Z]) has flow rate=(\d+); tunnels? leads? to valves? ([A-Z, ]+)/
+    let [_, name, flow, to, ...rest] = re.exec(line);
+    return {name, flow: +flow, to: to.split(', ').map(t=>+t)}
+} 
+
+const file = readFileSync("test", "utf-8");
+const lines = file.split("\n").filter(l=>l.trim() != '').map(parseLine);
+
+console.log(file)
