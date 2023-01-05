753a6996d8110ae83f3f2ac447e2dc33fd784d96
jotinha <dev@jgsousa.com>
2023-01-05 15:36:14 +0000

    17: working sim for 1 piece

--- 17/main.php
+++ 17/main.php
@@ -13,37 +13,36 @@ $pieces = [ # start at two units from left wall
     0b00110000_00110000
 ];
 
-function move_left($piece) { return array_map('blshift', $piece);}
-function move_right($piece) { return array_map('brshift', $piece);}
-
 function hits_wall($piece) { 
     $wall = 0x1010101; # same as 1 | (1<<8) | (1<<16) | (1<<24); 
     return ($piece & $wall) > 0;
 };
 
 function hits_world($piece, &$world, $y) {
-    foreach($piece as $i => $row) {
+    
+    for ($i=0; $i < 4; $i++) {
+        $row = $piece & 0b1111_1111; # mask last row
         if ((($world[$y+$i] ?? 0) & $row) > 0) return true;
+        $piece <<= 8; # scroll the piece down one line
     } 
     return false;
 }
 
 function place($piece, &$world, $y) {
-    foreach($piece as $i => $row) {
-        $world[$y+$i] = ($world[$y+$i] ?? 0 ) | $row; 
+    for ($i=0; $i < 4; $i++) {
+        $row = $piece & 0b1111_1111; # mask last row
+        $world[$y+$i] = ($world[$y+$i] ?? 0 ) | $row;
+        $piece <<= 8; # scroll the piece down one line
     }
 } 
 
 function simulate_one($piece, &$world, &$moves, $height=0) {
     $y = $height + 3; 
-    echo "move: ", $moves->valid() . "," . $moves->current();
     while($moves->valid() ) { 
         $wind = $moves->current();
         $moves->next();
 
-        echo "$wind y=$y ".join(",",array_map('decbin',$piece))."\n";
-
-        $candidate = $wind == '<' ? move_left($piece) : move_right($piece);
+        $candidate = $wind == '<' ? $piece << 1 : $piece >> 1;
         if (!hits_wall($candidate) and !hits_world($candidate, $world, $y)) { 
             $piece = $candidate;
         }
@@ -55,7 +54,8 @@ function simulate_one($piece, &$world, &$moves, $height=0) {
         $y--; 
         if ($y < 0 or hits_world($piece, $world, $y)) {
             place($piece, $world, $y+1);
-            return max($y+1+count($piece),$height);
+            return $y+1;
+            #FIXME return max($y+1+count($piece),$height);
         }
         $dworld = array_pad($world,10,0);
         place($piece, $dworld, $y);
@@ -66,7 +66,7 @@ function simulate_one($piece, &$world, &$moves, $height=0) {
 function display(&$world) {
     
     for ($i = max(array_keys($world)); $i >= 0; --$i) {
-        $row = ($world[$i] ?? 0) | (1<<7);
+        $row = ($world[$i] ?? 0) | (1<<8) | 1;
         $line = strtr(decbin($row),"01",".@");
         $line[0] = '|'; $line[8] = '|';
         echo $line."\n"; 
@@ -74,7 +74,7 @@ function display(&$world) {
     echo "+-------+\n";
 
 }
-function solve1(&$pieces, &$world, $moves) {
+function solve(&$pieces, &$world, $moves, $n) {
     $moves_iter = new InfiniteIterator(new ArrayIterator($moves));
     $pieces_iter = new InfiniteIterator(new ArrayIterator($pieces));
     
@@ -83,7 +83,7 @@ function solve1(&$pieces, &$world, $moves) {
     $moves_iter->rewind();
     foreach($pieces_iter as $piece) {
         $y = simulate_one($piece, $world, $moves_iter, $y);
-        if (++$i == 2022) break;
+        if (++$i == $n) break;
     }
     return $y;
 }
@@ -95,14 +95,13 @@ function draw($piece) {
 }
     
 
-#$ans1 = solve1($pieces, $world, $moves);
+$ans1 = solve($pieces, $world, $moves, 1);
 #$ans2 = 0;
 
-#display($world);
+display($world);
 #echo "$ans1,$ans2\n";
 $wall = 0x1010101; # same as 1 | (1<<8) | (1<<16) | (1<<24); 
-draw($wall);
-foreach($pieces as $p) {echo "\n";draw($p>>8);}
+#foreach($pieces as $p) {echo "\n";draw($p>>8);}
 foreach($pieces as $p) assert(!hits_wall($p));
 foreach($pieces as $p) assert(!hits_wall($p << 1));
 foreach($pieces as $p) assert(!hits_wall($p << 2));
