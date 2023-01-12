051e04e0cfa9d6659da40d1dddd5f6cf33711e98
jotinha <dev@jgsousa.com>
2023-01-12 16:43:03 +0000

    18: parse input

--- 18/main.clj
+++ 18/main.clj
@@ -1,1 +1,12 @@
-(println "Hello world")
+(require '[clojure.string :as str])
+
+(defn parse-line [line]
+  (map #(Integer/parseInt %) (str/split line #","))
+  )
+
+(def voxels 
+  (map parse-line
+    (str/split-lines (slurp "test"))))
+
+(prn voxels)
+;(run! println voxels)
