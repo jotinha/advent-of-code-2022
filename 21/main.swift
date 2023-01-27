4022764b5da81654922892be3fc17ee9bf02db0a
jotinha <dev@jgsousa.com>
2023-01-27 23:38:53 +0000

    21: solution part I

--- 21/main.swift
+++ 21/main.swift
@@ -11,10 +11,10 @@ func parse(command: String) -> Cmd {
     let tokens = command 
                 .trimmingCharacters(in: .whitespaces)
                 .components(separatedBy: .whitespaces)
-    if tokens.count == 1 {
-        return .n(Double(tokens[0])!)
-    } else {
-        return .op(tokens[1], tokens[0], tokens[2]) 
+    switch tokens.count {
+        case 1: return .n(Double(tokens[0])!)
+        case 3: return .op(tokens[1], tokens[0], tokens[2]) 
+        default: fatalError("invalid command string: \(command)")
     }
 }
 
@@ -22,10 +22,28 @@ func parse(file: String)  {
     let lines = try! String(contentsOfFile: file, encoding: .utf8)
                     .components(separatedBy: .newlines) 
     for line in lines {
-        let word_cmd = line.components(separatedBy: ":")
-        d[word_cmd[0]] = parse(command: word_cmd[1])
+        if !line.isEmpty {
+            let word_cmd = line.components(separatedBy: ":")
+            d[word_cmd[0]] = parse(command: word_cmd[1])
+        }
     }
 }
 
-parse(file: "test")
-print(d)
+func execute(_ word: String) -> Double {
+    switch d[word] {
+        case let .n(n) : return n
+        case let .op("+", w1, w2): return execute(w1) + execute(w2)
+        case let .op("-", w1, w2): return execute(w1) - execute(w2)
+        case let .op("/", w1, w2): return execute(w1) / execute(w2)
+        case let .op("*", w1, w2): return execute(w1) * execute(w2)
+        default: fatalError("unexpected cmd")
+    }    
+}
+
+parse(file: "input")
+let ans1 = Int(execute("root"))
+let ans2 = 0
+
+assert(ans1==194501589693264)
+
+print("\(ans1),\(ans2)")
