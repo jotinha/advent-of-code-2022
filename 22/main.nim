e46fe5af670287ed4f72d95afb321d9098865623
jotinha <dev@jgsousa.com>
2023-02-02 23:56:57 +0000

    22: parse file

--- 22/main.nim
+++ 22/main.nim
@@ -1,1 +1,13 @@
-echo "hello world"
+import std/strutils
+import std/re
+
+const
+   fname = "test"
+
+let
+   lines = readFile(fname).splitLines()
+   board = lines[0..^4] # range is inclusive, the last line is empty line
+   moves = findAll(lines[^2], re"([0-9]+|R|L)")
+
+echo moves 
+
