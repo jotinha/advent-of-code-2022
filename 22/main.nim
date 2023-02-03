85df526c5af0be498dbcd6455527aa88d7c883b9
jotinha <dev@jgsousa.com>
2023-02-03 00:12:35 +0000

    22: add basic types

--- 22/main.nim
+++ 22/main.nim
@@ -1,13 +1,30 @@
 import std/strutils
 import std/re
 
+type
+   Vec3 = array[3, float] 
+   State = object
+      pos: Vec3 # our position in the world
+      fwd: Vec3 # where we're looking at
+      up: Vec3 # where up is, same as the normal of the face of the map we're at
+               # for the first puzzle, the world is flat so this is always +y
+
+   Face = object
+      n: Vec3 # normal
+      u: Vec3 # the right direction of face in the texture map
+      v: Vec3 # the down direction of the face in the texture map
+      row0: int # row in the texture where the face starts
+      col0: int
+
 const
    fname = "test"
 
 let
    lines = readFile(fname).splitLines()
-   board = lines[0..^4] # range is inclusive, the last line is empty line
+   texture = lines[0..^4] # range is inclusive, the last line is empty line
    moves = findAll(lines[^2], re"([0-9]+|R|L)")
 
-echo moves 
+echo texture[10][15]
+
+
 
