31286a3d980fcee9399b3624ab8d16e924b301ef
jotinha <dev@jgsousa.com>
2023-02-11 00:22:29 +0000

    24: simpler loading and world struct

--- 24/main.d
+++ 24/main.d
@@ -1,36 +1,24 @@
-import std.stdio, std.string, std.algorithm;
+import std.stdio, std.string, std.algorithm, std.range;
 import std.container : DList;
 
-
-enum Dir { None, Up, Down, Left, Right};
 struct World {
-   Dir[128][32] pos;
-   int w, h;
+   string[] data; 
+   ulong h, w;
 };
 
 World world;
 
 void load(string fname) {
-   auto f = File(fname);
-   foreach(line; f.byLine) {
-      line = line[1..$-1];
-      if (line[1] != '#') {
-         foreach (w; line) {
-            Dir d;
-            switch (w) {
-                case '<': d = Dir.Left; break;
-                case '>': d = Dir.Right; break;
-                case '^': d = Dir.Up; break;
-                case 'v': d = Dir.Down; break;
-                case '.': d = Dir.None; break;
-                default: assert(0); 
-            }
-            world.pos[world.h++][world.w++] = d;
-         }
-      }
-   }
+   world.data = File(fname).byLineCopy
+                  .drop(1)
+                  .map!(line => line[1..$-1])
+                  .filter!(line => line[1] != '#')
+                  .array;
+   world.w = world.data[0].length;
+   world.h = world.data.length; 
 }
 
 void main() {
-    load("test");
+   load("test");
+   writeln(world);
 }
