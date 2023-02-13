e5287b2fa024077d2b159063ba3ed6cf1f7b9939
jotinha <dev@jgsousa.com>
2023-02-13 12:33:32 +0000

    25: use characters instead of ints

--- 25/main.ml
+++ 25/main.ml
@@ -3,22 +3,25 @@ let implode cs = String.of_seq (List.to_seq cs);;
 
 print_endline (implode (explode "Hello world"));;
 
+let dec = function | '0' -> 0 | '1' -> 1 | '2' -> 2 | '3' -> 3 | '4' -> 4
+let enc = function | 0 -> '0' | 1 -> '1' | 2 -> '2' | 3 -> '3' | 4 -> '4'
+
 let add1 x y c = 
-    let t = x+y+c in 
-    if t >= 5 then (t-5,1) else (t,0)
+    let t = (dec x) + (dec y) + c in 
+    if t >= 5 then (enc (t-5),1) else (enc t,0)
 ;;
 
 let rec addr xs ys carry = 
     match (xs,ys) with 
         | (x :: xs', y::ys') -> let (t,carry') = (add1 x y carry) in t :: addr xs' ys' carry' 
-        | ([],[]) -> if carry == 1 then [1] else []
-        | ([], _) -> addr [0] ys carry
-        | (_, []) -> addr xs [0] carry
+        | ([],[]) -> if carry == 1 then ['1'] else []
+        | ([], _) -> addr ['0'] ys carry
+        | (_, []) -> addr xs ['0'] carry
 ;;
 
 let add xs ys = List.rev (addr (List.rev xs) (List.rev ys) 0);;
 
-let print_list l = List.map (Printf.printf "%i,") l;;
+let print_list l = List.map (Printf.printf "%c,") l;;
 
-print_list (add [3] [4; 3]) 
+print_list (add ['3'] ['4'; '3']) 
 ;;
